9afd94829da7f548b39943765f8178721879797e
Timmynathan <oluwatimilehin.ilesanmi@pau.edu.ng>
2026-05-15 13:24:55 +0100

    Fix post render issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -92,7 +92,7 @@ export default function App() {
     setHeatmapUrl(null);
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://tb-detection-app-qjax.onrender.com', {
         method: 'POST',
         body: formData,
       });
